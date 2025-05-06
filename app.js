526f41196279001112ffc7cc61c4a7dbc0303d00
bijoymandal <bijoymandal00@gmail.com>
2025-05-06 23:21:14 +0530

    Refactor app.js to directly log sum and mean calculations, removing unnecessary Solution function

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-// Note:  Please do not change the pre-written code
+// app.js - our main application file
+const math = require("./math.js");
 
-// import the required module here
-const { sum, mean } = require('./math');
-const Solution = () => {
-    const nums = [1, 2, 3, 4, 5];
-    // write your code here to Display the results of the calculations on the console.
-    const totalSum = sum(nums);
-    const average = mean(nums);
-    console.log('The Sum is', totalSum);
-    console.log('The Mean is', average);
-};
-Solution();
-module.exports = Solution;
+const nums = [1, 2, 3, 4, 5];
+console.log(`The sum is ${math.sum(nums)}`);
+console.log(`The mean is ${math.mean(nums)}`);

a4ba7038d9dc44737e6919264f1925008125b0e0
bijoymandal <bijoymandal00@gmail.com>
2025-06-15 13:19:56 +0530

    feat: enhance product controller and layout for improved rendering and structure

--- mvc.js
+++ mvc.js
@@ -12,8 +12,15 @@ server.use(ejsLayouts);
 server.set('layout','layouts/main')
 //create an instance of productController
 const productController = new ProductController();
-server.get('/',productController.getProducts);
-
+server.get('/');
+//get All Products
+server.get('/product',productController.getProducts);
+//Add New Product
+server.get('/product/new-product',productController.getAddForm);
+//store New Product
+server.post('/product/add',productController.storeAddFrom);
+//middleware
+server.use(express.urlencoded({extended:true}));
 server.use(express.static('src/views'));
 
 server.listen(3400,()=>{
--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -8,7 +8,20 @@ export default class ProductController {
         let products = ProductModal.getProduct();
         console.log(products);
         console.log(path.resolve());
-        // return res.sendFile(path.join(path.resolve(), 'src', 'views', 'products.html'));
+        // return res .sendFile(path.join(path.resolve(), 'src', 'views', 'products.html'));
         res.render("pages/products",{title:'Product list',products:products})
+    }
+    getAddForm(req,res)
+    {
+        return res.render("pages/new-product",
+            {
+                layout:'layouts/main',
+                title:'Create Product',
+                subTitle:'Add New Product'
+            });
+    }
+    storeAddFrom(req,res){
+        console.log(req.body);
+        //res.redirect('/product');
     }
 }

e031cca78c1fa9e9c2e615aeebb099e02c718f80
bijoymandal <bijoymandal00@gmail.com>
2025-06-22 15:38:30 +0530

    Add new product route create

--- mvc.js
+++ mvc.js
@@ -2,14 +2,27 @@ import express from 'express';
 import ProductController from './src/controllers/product.controller.js';
 import ejsLayouts from 'express-ejs-layouts';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Fix __dirname in ES Module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = express();
+//middleware to parse urlencoded data
+server.use(express.urlencoded({extended:true}));
+// server.use(express.static('src/views'));
+
+
 //server view engine setup
 server.set('view engine', 'ejs');
-server.set('views', path.join(path.resolve(), 'src', 'views'));
+server.set('views', path.join(__dirname, 'src', 'views'));
 //use layouts Middleware in ejs Layouts
 server.use(ejsLayouts);
-server.set('layout','layouts/main')
+server.set('layout','layouts/main');
+server.use(express.json());
+server.use(express.static(path.join(__dirname, 'src', 'views')));
+
 //create an instance of productController
 const productController = new ProductController();
 server.get('/');
@@ -19,9 +32,8 @@ server.get('/product',productController.getProducts);
 server.get('/product/new-product',productController.getAddForm);
 //store New Product
 server.post('/product/add',productController.storeAddFrom);
-//middleware
-server.use(express.urlencoded({extended:true}));
-server.use(express.static('src/views'));
+
+
 
 server.listen(3400,()=>{
     console.log('server is running port',3400);

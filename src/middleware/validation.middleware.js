b4a0089f7dbf37609e9d50f64d4e997a7834c7ab
bijoymandal <bijoymandal00@gmail.com>
2025-06-24 07:48:55 +0530

    Refactor validation middleware to use express-validator for improved error handling and validation rules

--- src/middleware/validation.middleware.js
+++ src/middleware/validation.middleware.js
@@ -1,7 +1,7 @@
+import { body, validationResult } from "express-validator";
 
-
-const validRequest = (req, res, next) => {
-    const { name, price, desc,imageUrl } = req.body;
+const validRequest = async (req, res, next) => {
+    /*const { name, price, desc,imageUrl } = req.body;
         let errors=[];
         if(!name || name.trim() === ''){
             errors.push("Name is required");
@@ -22,13 +22,27 @@ const validRequest = (req, res, next) => {
         catch(err){
             console.log(err);
             errors.push("Image URL is not valid");
-        }
-        if(errors.length > 0){
+        }*/
+       //1. Set uo for validation
+       const rules = [
+        body('name').isEmpty().withMessage('Name is required'),
+        body('price').isEmpty().isFloat({gt:0}).withMessage('Price should be a positive number'),
+        body('desc').isEmpty().withMessage('Description is required'),
+        body('imageUrl').isURL().withMessage('Image URL is Invalid'),
+       ];
+
+       //2. Run the validation rules
+       await Promise.all(rules.map((rule)=>rule.run(req)));
+
+       //3. check if there are any validation errors
+       var errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
             return res.render("pages/new-product", {
                 layout: 'layouts/main',
                 title: 'Create Product',
                 subTitle: 'Add New Product',
-                errorMessage: errors[0],
+                errorMessage: errors.array()[0].msg,
                 // productData: req.body
             });
         }

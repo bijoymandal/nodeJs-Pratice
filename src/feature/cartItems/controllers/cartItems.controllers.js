a81ed3e81a62664edc223aeb5cde2864ffd4e122
bijoymandal <bijoy.mandal@mydevteam.com>
2025-08-27 20:55:57 +0530

    user and project wise cart update quantity and show user wise availablity cart items

--- src/feature/cartItems/controllers/cartItems.controllers.js
+++ src/feature/cartItems/controllers/cartItems.controllers.js
@@ -3,15 +3,14 @@ import CartItemsModel from "../model/cartItems.model.js";
 export class CartItemsController {
     add(req, res) {
         const {productID, quantity} = req.query;
-        const userID = req.userID;
-        // console.log("userID",userID,productID, quantity);
-        console.log("cartItems",CartItemsModel);
-        CartItemsModel.add(productID,userID, quantity);
-        res.status(200).json({success:true,message:"Product added to cart successfully"});
+        const userID = req.user.userID;
+        const updatedCart = CartItemsModel.add(productID,userID, quantity);
+        res.status(200).json({success:true,message:"Product added to cart successfully",cartItem:updatedCart});
     }
     get(req, res) {
-        const userID = req.userID;
+        const userID = req.user.userID;
         const cartItems = CartItemsModel.get(userID);
         res.status(200).json({success:true,cartItems:cartItems});
     }
+    
 }
--- src/feature/cartItems/model/cartItems.model.js
+++ src/feature/cartItems/model/cartItems.model.js
@@ -1,29 +1,33 @@
 export default class CartItemsModel {
     constructor(userID,productID, quantity,id) {
-        this.userID = userID;
-        this.productID = productID;
-        this.quantity = quantity;
-        this.id = id;
+        this.userID = Number(userID);
+        this.productID = Number(productID);
+        this.quantity = Number(quantity);
+        this.id = id?? cartItems.length + 1;
     }
 
     static add(productID,userID, quantity) {
-        /*const existingItem = cartItems.find(
-            (item) => item.productID === productID && item.userID === userID
+
+        const existingItem = cartItems.find(
+            (item) => item.productID == Number(productID) && item.userID == Number(userID)
         );
         if (existingItem) {
-            existingItem.quantity += quantity;
+            existingItem.quantity += Number(quantity);
+            return existingItem;
         } else {
             const newItem = new CartItemsModel(userID,productID, quantity);
             cartItems.push(newItem);
-        }*/
-       const cartItem = new CartItemsModel(userID,productID, quantity);
-       cartItem.id = cartItems.length + 1; // Simple ID generation
-       cartItems.push(cartItem);
-       return cartItem;
+            return newItem;
+
+        }
+    //    const cartItem = new CartItemsModel(userID,productID, quantity);
+    //    cartItem.id = cartItems.length + 1; // Simple ID generation
+    //    cartItems.push(cartItem);
+    //    return cartItem;
     }
 
     static get(userID) {
-        return cartItems.filter((item) => item.userID == userID);
+        return cartItems.filter((item) => item.userID == Number(userID));
     }
 }
 
@@ -31,5 +35,5 @@ export default class CartItemsModel {
 let cartItems = [
     new CartItemsModel(1,1,2,1),
     new CartItemsModel(2,2,1,2),
-    new CartItemsModel(3,3,4,3),
+    new CartItemsModel(1,2,4,3),
 ];

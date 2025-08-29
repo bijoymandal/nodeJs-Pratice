723fa043ddf884ad73654a4a3de01044bb72222f
bijoymandal <bijoy.mandal@mydevteam.com>
2025-08-29 19:22:17 +0530

    Add Swagger documentation and integrate swagger-ui-express

--- api.js
+++ api.js
@@ -1,15 +1,25 @@
+import fs from "fs";
 import express from "express";
+import swagger from "swagger-ui-express";
+// import apiDocs from './swagger.json' assert { type: 'json' };
+
+
+
 import userRouter from "./src/feature/user/user.routes.js";
 import jwtAuth from "./src/middleware/jwt.middleware.js";
 import productRouter from "./src/feature/product/routes/product.routes.js";
 import cartItemsRouter from "./src/feature/cartItems/routes/cartItems.route.js";
+
+const apiDocs = JSON.parse(
+  fs.readFileSync(new URL("./swagger.json", import.meta.url), "utf-8")
+);
 
 const server = express();
 server.use(express.json());
 server.get("/", (req, res) => {
   res.send("Welcome to Ecommerce API");
 });
-
+server.use('/api-docs', swagger.serve, swagger.setup(apiDocs));
 server.use("/api/product",jwtAuth,productRouter);
 server.use("/api/users" ,userRouter);
 server.use('/api/cartItems',jwtAuth,cartItemsRouter);

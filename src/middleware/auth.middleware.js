ddd9e2aec707e1e19e6c357e54ea4cc2e2e2e798
bijoymandal <bijoy.mandal@mydevteam.com>
2025-09-29 20:53:53 +0530

    Refactor auth middleware to check for user session and improve redirection logic

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,7 +1,7 @@
 export const authMiddleware = (req, res, next) => {
-  if (req.session.userEmail) {
-    next(); // User is authenticated, proceed to the next middleware or route handler
+  if (req.session && req.session.user) {
+    return next(); // User is authenticated, proceed to the next middleware or route handler
   } else {
-    res.redirect("/user/login"); // User is not authenticated, redirect to login page
+    return res.redirect("/"); // User is not authenticated, redirect to login page
   }
 };

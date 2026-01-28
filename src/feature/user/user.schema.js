504c550ad7cf9beaabc7f8a0356a1a6cc4d3de22
Bijoy Mandal <30389880+bijoymandal@users.noreply.github.com>
2026-01-28 23:02:39 +0530

    Enhance user schema with email and password validation
    Updated email validation regex and password validation rules.

--- src/feature/user/user.schema.js
+++ src/feature/user/user.schema.js
@@ -3,7 +3,16 @@ import mongoose from "mongoose";
 
 export const userSchema = new mongoose.Schema({
     name:String,
-    email:{type:String,unique:true},
-    password:String,
+    email:{type:String,unique:true,match.[/\,"Please enter the valid email"]},
+    password:{
+        type"String,
+        validate:{
+            validator:function(value}
+            {
+                return /..\.test(value)            
+            },
+            message:"password should be 8-12 characters and have a special characters"
+    }
+},
     type:{type:String,enum:['Customer','Seller']},
 });
